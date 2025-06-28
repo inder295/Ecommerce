1f2df60243b8b2e01059460471d5fbfd0309d10c
INDERPREET SINGH <inderpreetsingh2017@gmail.com>
2025-06-28 17:31:26 +0530

    Welcome page completed

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,8 +2,32 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div className='h-50 '>
-      
+    <div className='h-50 grid grid-cols-3 gap-10 p-10 pl-15 font-serif'>
+
+        <div className='text-xl'>
+           
+            <ul>Our Colection</ul>
+            <li className='cursor-pointer hover:text-amber-700'>Formal</li>
+            <li className='cursor-pointer hover:text-amber-700'>Sports</li>
+            <li className='cursor-pointer hover:text-amber-700'>Leads</li>
+        </div>
+        <div className='text-2xl'>
+            <p>Subscripe to Our Newsletter</p>
+           <input
+  type="text"
+  placeholder="Enter your Email here"
+  class="w-90 max-w-md px-4 py-2 mt-3 text-base border border-gray-300 rounded  focus:outline-none focus:ring-2 "
+/>
+              <button className='bg-amber-200 px-4 py-2 rounded-xl mt-4 w-50 h-10 cursor-pointer hover:bg-amber-300 text-lg font-bold font-serif'>Subscribe</button>
+        </div>
+        <div className='text-xl'>
+            <ul>Help and Support</ul>
+            <li className='cursor-pointer hover:text-amber-700'>Contact Us</li>
+            <li className='cursor-pointer hover:text-amber-700'>FAQ</li>
+            <li className='cursor-pointer hover:text-amber-700'>Shipping And Delivery</li>
+            <li className='cursor-pointer hover:text-amber-700'>User Guide</li>
+        </div>
+        
     </div>
   )
 }
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import Search from './Search'
 
 function Header() {
-  return (
-    <div className='h-15'>
-      
+  return <>
+      <div className='h-20 flex justify-between p-5 shadow-md px-20'>
+        <div className='font-serif font-bold text-3xl font-style: italic cursor-pointer p-2 '>Shopme</div>
+        
+        <div className='flex p-2 space-x-10 text-xl '>
+            <p className='cursor-pointer hover:text-red-600'>About</p>
+            <p className='cursor-pointer hover:text-red-600'>Market</p>
+        </div>
+
+        <div className='flex space-x-5'>
+            
+          
+            <img src="cart.jpg" alt="cart" className='border-1 border-solid rounded-sm cursor-pointer' />
+            <img src="profile.png" alt="profile" className='h-10 pt-0 align-content: center cursor-pointer ' />
+            
+
+        </div>
     </div>
-  )
+   
+</>
+
+   
+  
 }
 
 export default Header
