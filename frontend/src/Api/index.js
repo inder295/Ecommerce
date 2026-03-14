11d50edf1de416b43cd4a9eacbcf2deb80dcaae3
INDERPREET SINGH <inderpreetsingh2017@gmail.com>
2026-03-15 02:11:46 +0530

    remove the blockings

--- frontend/src/Api/index.js
+++ frontend/src/Api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? 'http://localhost:3000/api/v1': "/api",
+  baseURL: import.meta.env.MODE === "development" ? 'http://localhost:3000/api/v1': "/api/v1",
   withCredentials: true,
 });
 
